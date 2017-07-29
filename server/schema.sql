5be3b8ddb6964927a544aa5220620a60ab375c56
Scott Schaefer <scottschaef@gmail.com>
2017-07-29 10:36:41 -0400

    Created messages table

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,12 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  message_id int(11) not null AUTO_INCREMENT,
+  message varchar(45) default null,
+  created DATE default null,
+  PRIMARY KEY (message_id),
+  FOREIGN KEY (user_id) references users(user_id),
+  FOREIGN KEY (room_id) references rooms(room_id)
 );
 
 /* Create other tables and define schemas for them here! */
